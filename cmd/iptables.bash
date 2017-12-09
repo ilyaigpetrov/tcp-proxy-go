c2bc4ae2b9bbcdc66d4b7896b5eea79feb057a88
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-12-09 19:34:26 +0500

    Add build script

--- cmd/iptables.bash
+++ cmd/iptables.bash
@@ -1,4 +1,3 @@
-sudo useradd proxyrunner
 sudo iptables -t nat -A OUTPUT -m tcp -p tcp --dport 80 -m owner --uid-owner proxyrunner -j RETURN
 sudo iptables -t nat -A OUTPUT -m tcp -p tcp --dport 443 -m owner --uid-owner proxyrunner -j RETURN
 sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 1111
