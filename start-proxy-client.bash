69e627cdae8e2f7d13ab850a92df244b33f2674b
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-12-10 12:57:31 +0500

    Client and server work on local machine

--- start-proxy-client.bash
+++ start-proxy-client.bash
@@ -2,4 +2,4 @@
 sudo useradd proxyclient
 sudo iptables -t nat -A OUTPUT -m tcp -p tcp --dport 80 -m owner --uid-owner proxyclient -j RETURN
 sudo iptables -t nat -A OUTPUT -m tcp -p tcp --dport 443 -m owner --uid-owner proxyclient -j RETURN
-sudo -u proxyrunner ./proxy-client $@
+sudo -u proxyclient ./proxy-client $@
